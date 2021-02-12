38ee6125e9b350e774c6bc43b71c6c3bdf81dfd8
Nick Richmond <get@nickrichmond.dev>
2021-02-12 11:20:53 -0500

    configure relative imports in jest

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+function makeModuleNameMapper(srcPath, tsconfigPath) {
+  // Get paths from tsconfig
+  const { paths } = require(tsconfigPath).compilerOptions
+
+  const aliases = {}
+
+  // Iterate over paths and convert them into moduleNameMapper format
+  Object.keys(paths).forEach(item => {
+    const key = item.replace('/*', '/(.*)')
+    const path = paths[item][0].replace('/*', '/$1')
+    aliases[key] = srcPath + '/' + path
+  })
+
+  return aliases
+}
+
+const TS_CONFIG_PATH = './tsconfig.json'
+const SRC_PATH = '<rootDir>/src'
+
+module.exports = {
+  rootDir: './',
+  testMatch: ['<rootDir>/test/*.test.ts'],
+  moduleNameMapper: makeModuleNameMapper(SRC_PATH, TS_CONFIG_PATH)
+}
